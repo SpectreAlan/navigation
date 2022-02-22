f1c3bec8aa99057b7592cfc0906660a0e9256129
PC-20220124AQUF\Administrator <lidenggui@dgg.net>
2022-02-22 17:11:44 +0800

    删除vant库

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,13 +17,10 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-    'vant/lib/index.css'
-  ],
+  css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    '@/plugins/vant',
     { src: '@/plugins/store', ssr: false }
   ],
 
