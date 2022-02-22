4b9b84c21375e37c0e4d5c35f78df988174feeba
PC-20220124AQUF\Administrator <lidenggui@dgg.net>
2022-02-22 16:47:33 +0800

    init project navigation

---
+++ pages/about.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="about">
+    <h2>about</h2>
+    <h2>{{info.age}}</h2>
+    <NuxtLink to="/">Home page</NuxtLink>
+  </div>
+</template>
+
+<script>
+ export default {
+   name: 'About',
+   computed: {
+     info () {
+       return this.$store.state.user.info
+     }
+   }
+ }
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="container">
+    <h2>Home</h2>
+    <NuxtLink to="/about">About page</NuxtLink>
+    <button @click="setStore">设置</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+  methods: {
+    setStore () {
+      this.$store.commit('user/setInfo', {name: 'jack', age: 18})
+    }
+  }
+}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family:
+    Quicksand,
+    "Source Sans Pro",
+    -apple-system,
+    BlinkMacSystemFont,
+    "Segoe UI",
+    Roboto,
+    "Helvetica Neue",
+    Arial,
+    sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
