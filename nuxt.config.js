6b54d4f7e9c2c2836cf4f904e44919f5cfc3496b
PC-20220124AQUF\Administrator <lidenggui@dgg.net>
2022-03-07 19:47:36 +0800

    配置css reset

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,7 +18,8 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
-    {src:'element-ui/lib/theme-chalk/index.css'}
+    {src:'element-ui/lib/theme-chalk/index.css'},
+    'static/css/reset.css'
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
